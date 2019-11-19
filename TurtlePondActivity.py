b5590eb0536f1d76bbc9869d8fff60b9292f9a3e
Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>
2019-11-19 12:32:20 +0100

    fix PyGIWarnings

--- TurtlePondActivity.py
+++ TurtlePondActivity.py
@@ -10,6 +10,9 @@
 # along with this library; if not, write to the Free Software
 # Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
+import gi
+gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk, GObject, Gdk
 
 import sugar3
